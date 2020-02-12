f81dd3b2ef42aa10112b0264ce4f9edcfc97c997
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2020-02-13 05:10:20 +0800

    Radio Buttons

--- src/main/java/com/springdemo/model/Student.java
+++ src/main/java/com/springdemo/model/Student.java
@@ -7,8 +7,9 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
-
     private LinkedHashMap<String, String> countryOptions;
+    private String favoriteLanguage;
+
     public Student() {
         countryOptions = new LinkedHashMap<>();
 
@@ -45,5 +46,13 @@ public class Student {
 
     public LinkedHashMap<String, String> getCountryOptions() {
         return countryOptions;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 }
